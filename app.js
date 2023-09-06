902a6d95292745343137828ccd0954c54556019f
Ezequiel Cabras <ezequiel_cabras-9712@hotmail.com>
2023-09-06 12:48:15 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const MetaProvider = require('@bot-whatsapp/provider/meta')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+/**
+ * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
+ *
+ *          Menu Principal
+ *           - SubMenu 1
+ *             - Submenu 1.1
+ *           - Submenu 2
+ *             - Submenu 2.1
+ *
+ * Primero declaras los submenus 1.1 y 2.1, luego el 1 y 2 y al final el principal.
+ */
+
+const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+
+const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
+    [
+        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
+        'https://bot-whatsapp.netlify.app/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
+    [
+        '🙌 Aquí encontras un ejemplo rapido',
+        'https://bot-whatsapp.netlify.app/docs/example/',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
+    [
+        '🚀 Puedes aportar tu granito de arena a este proyecto',
+        '[*opencollective*] https://opencollective.com/bot-whatsapp',
+        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
+        '[*patreon*] https://www.patreon.com/leifermendez',
+        '\n*2* Para siguiente paso.',
+    ],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowDiscord = addKeyword(['discord']).addAnswer(
+    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
+    null,
+    null,
+    [flowSecundario]
+)
+
+const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
+    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
+    .addAnswer(
+        [
+            'te comparto los siguientes links de interes sobre el proyecto',
+            '👉 *doc* para ver la documentación',
+            '👉 *gracias*  para ver la lista de videos',
+            '👉 *discord* unirte al discord',
+        ],
+        null,
+        null,
+        [flowDocs, flowGracias, flowTuto, flowDiscord]
+    )
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([flowPrincipal])
+
+    const adapterProvider = createProvider(MetaProvider, {
+        jwtToken: process.env.JWTOKEN, //EAAH7WLPZAWukBOww40W4UZA5STe33dPPlp3euMbNZCG5gc4XK7iv6NR6YGLGgWR0SeFtDdDaZBrZAijMMzjrKFlQu3a5URBp5EBWuZBTrZBIjKxut1n4P8U7GAqdDwVCyU5hlj2ZAq56b6h2gMZBs1ulZAoiVLNl55yCy4Lk4MZAusc1VjJmvy7fZBR7wQEZCf2ZAIR0LIP70YybC2x7YqRfwl431csV9uDJAZD
+        numberId: process.env.NUMBER_ID, //128667113656028
+        verifyToken: process.env.VERIFY_TOKEN, //654ersdf65g4a32rt15dsf3sd1f15awer6sdfuiAEAST5A45
+        version:'v16.0'
+    })
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+}
+
+main()
